97984cda4817033d0ba7a5a88660b4378154788d
MICHAL_URBANSKI <michal.urbanski@onet.pl>
2020-08-27 12:26:15 +0200

    Created Driver Class with basic data and test of specified Class

--- src/main/java/org/project/service/bus/main/Driver.java
+++ src/main/java/org/project/service/bus/main/Driver.java
@@ -1,4 +1,75 @@
 package org.project.service.bus.main;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Driver {
+    @Id
+    @GeneratedValue
+    private int driver_id;
+
+    @Column(nullable = false)
+    private String driver_name;
+
+    @Column(nullable = false)
+    private String driver_last_name;
+
+    @Column(nullable = false)
+    private Long driver_number;
+
+    public Driver() {
+    }
+
+    public Driver(String driver_name, String driver_last_name, Long driver_number) {
+        this.driver_name = driver_name;
+        this.driver_last_name = driver_last_name;
+        this.driver_number = driver_number;
+    }
+
+    public int getDriver_id() {
+        return driver_id;
+    }
+
+    public void setDriver_id(int driver_id) {
+        this.driver_id = driver_id;
+    }
+
+    public String getDriver_name() {
+        return driver_name;
+    }
+
+    public void setDriver_name(String driver_name) {
+        this.driver_name = driver_name;
+    }
+
+    public String getDriver_last_name() {
+        return driver_last_name;
+    }
+
+    public void setDriver_last_name(String driver_last_name) {
+        this.driver_last_name = driver_last_name;
+    }
+
+    public Long getDriver_number() {
+        return driver_number;
+    }
+
+    public void setDriver_number(Long driver_number) {
+        this.driver_number = driver_number;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "driver_id=" + driver_id +
+                ", driver_name='" + driver_name + '\'' +
+                ", driver_last_name='" + driver_last_name + '\'' +
+                ", driver_number='" + driver_number + '\'' +
+                '}';
+    }
+
+
 }
--- src/test/java/org/project/service/bus/main/DriverTest.java
+++ src/test/java/org/project/service/bus/main/DriverTest.java
@@ -1,7 +1,95 @@
 package org.project.service.bus.main;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DriverTest {
+
+    private static SessionFactory sessionFactory;
+
+    @BeforeAll
+    static void setup() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        sessionFactory = new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+    }
+
+    @Test
+    void addedDriverIntoDatabaseShouldSaveAfterTransaction() {
+
+        //given
+
+        Driver driver = new Driver("Maciej", "Kowalski", 50230L);
+        Driver driver1 = new Driver("Michał", "Wąs", 50235L);
+        //when
+        try {
+            Session session = sessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.persist(driver);
+            session.persist(driver1);
+            tx.commit();
+        } catch (HibernateException e) {
+            System.out.println("Error during adding Driver to Database!");
+            e.printStackTrace();
+        }
+        //then
+        System.out.println("Success ! Added new Driver to Database");
+
+    }
+
+    @Test
+    void addedDriverToDatabaseShouldBeReadable() {
+        //given
+        Driver driver = new Driver("Maciej", "Kowalski", 50230L);
+        Driver driver1 = new Driver("Michał", "Wąs", 50235L);
+        try {
+            Session session = sessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.persist(driver);
+            session.persist(driver1);
+            tx.commit();
+        } catch (HibernateException e) {
+            System.out.println("Error during adding Driver to Database!");
+            e.printStackTrace();
+        }
+
+        //when
+        try {
+            Session session = sessionFactory.openSession();
+            List<Driver> list = session.createQuery("from Driver",Driver.class).list();
+
+            //then
+            assertThat(list.size(),notNullValue());
+
+            System.out.println("List of Drivers:\n");
+
+            for (Driver el :
+                    list) {
+                System.out.println(el);
+            }
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
